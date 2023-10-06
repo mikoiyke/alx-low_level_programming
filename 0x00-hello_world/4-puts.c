ee750030369ad27637a0c451e1748835417cad67
Michael Onyekwere <mikoiyke@gmail.com>
2023-10-05 20:01:32 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
+#include <stdio>
 
 /**
-  * main - print valve
-  * Description: Print input
+  * main - A program that prints a line + funtion
   * Return: Returns 0
-*/
+  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle);
-	return (0);
-}
+put("\"Programming is like building a multilingual puzzle");
+return (0);
