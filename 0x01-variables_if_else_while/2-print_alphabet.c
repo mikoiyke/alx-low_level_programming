71916a35c5cfec4ec2295a84748f24ef808f734b
Michael Onyekwere <mikoiyke@gmail.com>
2023-10-08 12:49:02 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - Entry
+  * Description: trail
+  * Return: sucess (0)
+  */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+	for (i = 0; i < 26; i++)
+{
+	putchar(alp[i]);
+}
+	putchar('\n');
+	return (0);
+}
