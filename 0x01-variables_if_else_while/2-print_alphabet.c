bf2bb3d896b8fb7bae5c078191f1ad435b3586c9
Michael Onyekwere <mikoiyke@gmail.com>
2023-10-08 13:16:49 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 /**
-  * main - Entry
+  * main - Entry point
   * Description: trail
-  * Return: sucess (0)
+  * Return: success (0)
   */
 int main(void)
 {
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
+
 	for (i = 0; i < 26; i++)
-{
+	{
 	putchar(alp[i]);
-}
+	}
 	putchar('\n');
 	return (0);
 }
