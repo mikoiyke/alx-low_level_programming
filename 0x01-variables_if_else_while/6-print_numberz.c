31071daf1e350704c5549ea7a70cad9357637389
Michael Onyekwere <mikoiyke@gmail.com>
2023-10-09 08:59:48 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  * Return: success (0)
+*/
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n >= 10; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
