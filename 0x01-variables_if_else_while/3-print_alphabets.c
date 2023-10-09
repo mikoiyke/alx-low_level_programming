13a691037aea69595d4785b57f894c78f1a53101
Michael Onyekwere <mikoiyke@gmail.com>
2023-10-09 02:58:15 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  * main - entry DD
+  * Description: HH
+  * Return: success (0)
+  */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	putchar(letter);
+	putchar('\n');
+	return (0);
+}
