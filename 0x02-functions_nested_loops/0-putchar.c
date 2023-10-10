ce3e5aab9ef9d161b750f34841eaf57665d77a10
Michael Onyekwere <mikoiyke@gmail.com>
2023-10-10 10:21:12 -0700

    main.h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Print putchar
+ * Description: Print
+ * Return: Success (0)
+*/
+int print(_putchar);
+
+int main(void)
+{
+	char print = ("_putchar")
+
+	putchar(print);
+	return (0);
+}
