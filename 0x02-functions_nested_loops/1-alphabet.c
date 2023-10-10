03856522d7df2e6612562b086f0a81dd3f81f68f
Michael Onyekwere <mikoiyke@gmail.com>
2023-10-10 16:15:53 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * main - Entry point
+  * Description: Write a program that prints _putchar, followed by a new line
+  * Return: Success (0)
+  */
+int main(void)
+{
+	int x = 'a';
+
+	for (x = 'a'; x <= 'z'; a++)
+	_putchar(x);
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,2 @@
 char _putchar(char);
+char print_alphabet(char);
