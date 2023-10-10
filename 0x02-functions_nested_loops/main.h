6e0afc9ae03ca258316345c2c6f48d6163959142
Michael Onyekwere <mikoiyke@gmail.com>
2023-10-10 16:46:57 -0700

    3-islower.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 char _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,5 +14,3 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
-#endif
