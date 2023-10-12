9e3bac56c0d7ac04b58def4409233eeb70de6c42
Michael Onyekwere <mikoiyke@gmail.com>
2023-10-12 14:47:30 -0700

    check for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase character check
+ * @c: the character to be checked
+ *
+ * Return: 1 if c is uppercase and 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+ALX Functions and Nested Loops
