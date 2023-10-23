5bfdf2aaf085710e842348876c1f9dc5f9601704
Michael Onyekwere <mikoiyke@gmail.com>
2023-10-23 08:39:28 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * _memcpy - copies memory area
+  * @dest: first memory area
+  * @src: second memory area
+  * @n: the interval
+  * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x < n; x++)
+		dest[x] = src[x];
+	return (dest);
+}
