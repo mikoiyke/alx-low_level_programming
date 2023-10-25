f34bcdef61fd17a8fa175f919dcbfe3ea8a33dea
Michael Onyekwere <mikoiyke@gmail.com>
2023-10-24 18:59:57 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * set_string - main function
+  * @s: Function parameter 2
+  * @to: Function parameter 1
+  * Return: Always 0.
+  */
+void set_string(char **s, char *to)
+
+{
+	*s = to;
+}
