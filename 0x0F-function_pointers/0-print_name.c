aacb2491218b0763b614ff669f018d1cd7c4617b
Michael Onyekwere <mikoiyke@gmail.com>
2023-11-08 01:25:00 -0800

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,13 @@
 #include "function_pointers.h"
 
 /**
-
-  *
-  *
-  *
-  *"
-  *
-  */
+ * print_name - function that prints a name
+ * @name: name to be printed
+ * @f: pointer to function
+ * Return: nothing
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
-	_putchar("%s", &name);
+	f(name);
 }
