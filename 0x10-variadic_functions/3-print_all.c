c62869025812dd955ed4454d2e7d1f7e7d12325a
Michael Onyekwere <mikoiyke@gmail.com>
2023-11-09 15:49:41 -0800

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,4 @@
 #include "variadic_functions.h"
-
 /**
  * print_all - prints anything
  * @format: list of types of arguments passed to the function
@@ -21,8 +20,7 @@ void print_all(const char * const format, ...)
 	{
 		printf(", ");
 		break;
-	}
-	j++;
+	} j++;
 	}
 	switch (format[i])
 	{
